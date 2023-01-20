bc03aca97cdcee43ac53fc4ad087b3e087d4235c
Moe Martinez <moe@moemartinez.com>
2023-01-20 10:00:38 -0600

    url replace fix

--- node/index.js
+++ node/index.js
@@ -1,21 +1,21 @@
 const tmi = require('tmi.js');
 
 const handleMessage = async (message)=>{
-    let stopDefault = await fetch("http://0.0.0.0/sys", {
-                method: "POST",
-                body: new URLSearchParams({"status": "stop"})
-            });
-            console.log(await stopDefault)
+    // let stopDefault = await fetch("http://0.0.0.0/sys", {
+    //             method: "POST",
+    //             body: new URLSearchParams({"status": "stop"})
+    //         });
+            // console.log(await stopDefault)
     let sendMessage = await fetch("http://0.0.0.0/redemption",{
         method: "POST",
         body: new URLSearchParams({"redemption": message})
     })
-    console.log(await sendMessage)
-    let startDefault = await fetch("http://0.0.0.0/sys", {
-        method: "POST",
-        body: new URLSearchParams({"status": "start"})
-    }); 
-    console.log(startDefault)
+    // console.log(await sendMessage)
+    // let startDefault = await fetch("http://0.0.0.0/sys", {
+    //     method: "POST",
+    //     body: new URLSearchParams({"status": "start"})
+    // }); 
+    // console.log(startDefault)
 }
 
 
@@ -24,20 +24,21 @@ const client = new tmi.Client({
 });
 
 client.connect();
-console.log(client)
+
 client.on('message', (channel, tags, message, self) => {
     if(tags['custom-reward-id'] === "86b6fceb-becb-44fe-aa24-bed339f5d41e"){
     let emotes = new Map();
     if(tags['emotes'] !=null){
+            
             let start = 0;
             let end = 0;
             for (let [key, value] of Object.entries(tags['emotes'])) {
+
                 let url = `https://static-cdn.jtvnw.net/emoticons/v2/${key}/default/dark/2.0`;
-                console.log(url)
                 let splitIndex = value[0].split("-") 
                 
-                    start = parseInt(splitIndex[0])+1
-                    end = parseInt(splitIndex[1])+2
+                    start = parseInt(splitIndex[0])
+                    end = parseInt(splitIndex[1])+1
                 let newkey =  message.substring(start, end)
                 
                 emotes.set(newkey, url)
@@ -46,12 +47,12 @@ client.on('message', (channel, tags, message, self) => {
             }
 
             emotes.forEach((val, key, map)=>{
-                message = message.replace(key, val)
+                message = message.replaceAll(key, val)
             })
             
             
         }
-        
+        console.log(message)
         handleMessage(message)
 
     }
