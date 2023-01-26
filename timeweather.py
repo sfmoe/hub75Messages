af2c089b392ad383c3729b35f08b6055eb5a33fb
Moe Martinez <moe@moemartinez.com>
2023-01-26 14:34:53 -0600

    time weather widget run through cron

---
+++ timeweather.py
@@ -0,0 +1,100 @@
+# 1st row 9x 5y 12px font
+# 2nd row 9x 18y 12px font
+from PIL import Image, ImageDraw, ImageFont
+import os
+import datetime
+import pathlib
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+openweatherAPIKEY = os.getenv("openweather")
+
+
+import requests
+import json
+
+
+width=64
+height=32
+fontSize=12
+working_dir = pathlib.Path(__file__).parent.absolute()
+gif_folder = os.path.join(working_dir, "static/images/")
+font_folder = os.path.join(working_dir, "static/fonts")
+time_stage = "time_stage.gif"
+gif_name = "stage.gif"
+emojiFont = ImageFont.truetype(os.path.join(font_folder, "seguiemj.ttf"), fontSize)
+stage_x = 9
+lineone_y = 5
+linetwo_y = 18
+
+now = datetime.datetime.now()
+
+
+
+def makeStage():
+    image = Image.open(os.path.join(gif_folder, time_stage)).convert('RGBA')
+    return image
+
+def makeTime():
+    global emojiFont
+    frames = []
+    text = ''
+    hour = now.strftime('%-I')
+    minute = now.strftime('%M')
+    ampm = now.strftime('%p')
+    numb_frames = 2
+    linetwo = ""
+    min_temp = ""
+    max_temp = ""
+    if minute =="00" or minute=="15" or minute=="30" or minute=="45":
+        min_temp, max_temp = weather()
+        weatherCache = open('weathercache.txt', 'w')
+        linetwo = f"{min_temp}° / {max_temp}°"
+        weatherCache.write(linetwo)
+        weatherCache.close()
+    else:
+
+        weatherCache = open('weathercache.txt', 'r')
+        linetwo = weatherCache.read()
+    
+
+    while numb_frames > 0:
+        stage = makeStage()
+        layer = ImageDraw.Draw(stage)
+        if numb_frames == 2:
+            text = f"{hour}:{minute} {ampm}"
+        else:
+            text = f"{hour} {minute} {ampm}"
+        textwidth, textheight = emojiFont.getsize(text)
+        linetwow, linetwoh = emojiFont.getsize(linetwo)
+
+        new_x = int((width-textwidth)/2)
+        linetwo_fixedX = int((width-linetwow)/2)
+        layer.text( (new_x, lineone_y), text, font=emojiFont, embedded_color=True)
+        layer.text( (linetwo_fixedX, linetwo_y), linetwo, font=emojiFont, embedded_color=True)
+
+        frames.append(stage)
+        numb_frames -= 1
+    frame_one = frames[0]     
+    frame_one.save(os.path.join(gif_folder, "outgif", gif_name), format="GIF", append_images=frames, save_all=True, duration=400, loop=0)
+
+def weather():
+    temp_max = 0
+    temp_min = 0
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?lat=41&lon=-87&appid={openweatherAPIKEY}")
+    json_data = response.json() if response and response.status_code == 200 else None
+    if json_data and 'main' in json_data:
+        temp_max = kelvfar(json_data["main"]["temp_max"])
+        temp_min = kelvfar(json_data["main"]["temp_min"])        
+    return (temp_min, temp_max)
+
+def kelvfar(val):
+    # kelvin conversion formula
+    # (Kelvin − 273.15) × 1.8 + 32 = Farenheit
+    # round(temp)
+    conversion = (val - 273.15) * 1.8 + 32
+    return round(conversion)
+
+makeTime()
+
